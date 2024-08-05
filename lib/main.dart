2ec23cb16d6d5841c6ee540a6d8873f10ded1f1d
mkvirus <mkveee @gmail.com>
2024-08-05 10:42:33 +0100

    "Added Firebase messaging configuration and handling in AndroidManifest.xml and main.dart, and updated pubspec.yaml and pubspec.lock files accordingly."

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,67 @@
-
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inapp_notifications/flutter_inapp_notifications.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:logger/web.dart';
 import 'package:tak/firebase_options.dart';
 
 import 'config/theme/theme.dart';
 import 'features/onboard/presentation/pages/onboarding.dart';
 
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  //await Firebase.initializeApp();
+
+  Logger().d("Handling a background message: ${message.messageId}");
+}
+
 Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
+  );
+  requestNotificationPermission();
+  await FirebaseMessaging.instance.setAutoInitEnabled(true);
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
+  //Foreground messages
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    Logger().d('Got a message whilst in the foreground!');
+    Logger().d('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      Logger()
+          .d('Message also contained a notification: ${message.notification}');
+    }
+  });
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  Logger().d("FCM Token:  $fcmToken");
   runApp(const TakEstate());
 
   FlutterNativeSplash.remove();
+}
+
+Future<void> requestNotificationPermission() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: true,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  Logger().d('User granted permission: ${settings.authorizationStatus}');
 }
 
 class TakEstate extends StatelessWidget {
